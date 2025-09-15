fc0b37fc481eb33edab8857a22a32f68ee5d0c54
Aman Sayyad <91181377+ZaddyAman@users.noreply.github.com>
2025-09-15 10:04:47 +0530

    Update main.py

--- langchain_rag_engine/main.py
+++ langchain_rag_engine/main.py
@@ -63,7 +63,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.add_middleware(GZipMiddleware, minimum_size=1000)
-app.add_middleware(TrustedHostMiddleware, allowed_hosts=["localhost", "127.0.0.1", "*.localhost"])
+ app.add_middleware(TrustedHostMiddleware, allowed_hosts=["*"])
 
 # --- Static Files ---
 # Mount uploads directory to serve user-uploaded files (avatars, etc.)
